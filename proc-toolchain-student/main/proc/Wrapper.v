dafb24146d85472b86c680460feaf5e376200126
drinconcam364 <danilo.rinconcamacho@duke.edu>
2026-05-27 09:56:29 -0400

    Add files via upload

--- proc-toolchain-student/main/proc/Wrapper.v
+++ proc-toolchain-student/main/proc/Wrapper.v
@@ -35,7 +35,7 @@ module Wrapper (clock, reset);
 
 
 	// ADD YOUR MEMORY FILE HERE
-	localparam INSTR_FILE = "positive_no_bypass";
+	localparam INSTR_FILE = "";
 	
 	// Main Processing Unit
 	processor CPU(.clock(clock), .reset(reset), 
